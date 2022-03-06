81b07a49db8493bc1e80f41b02eaf64d215c7c6a
kstensing <stensingk@gmail.com>
2022-03-06 13:55:32 -0600

    added notes so when new note is added it's rendered to page

--- Develop/server.js
+++ Develop/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const noteData = require('./db/db.json');
-const uuid = require('uuid');
+// const noteData = require('./db/db.json');
+const uniqid = require('uniqid');
 
-//const res = require('express/lib/response');
+const res = require('express/lib/response');
 
 const PORT = 3001;
 
@@ -20,19 +20,64 @@ app.get('/notes', (req, res) => {
 });
 
 // one or the other:
-app.get('/api/notes', (req, res) => res.json(noteData));
+app.get('/api/notes', (req, res) => {
+    
+    fs.readFile('./db/db.json', 'utf8', (err, noteData) => {
+        if (err) {
+            console.error(err);
+        } 
+        
+        const allNotes = JSON.parse(noteData);
+        res.json(allNotes);
+    });
+});
 
 app.post('/api/notes', (req, res) => {
-    res.json(req.body);
 
+    const { title, text } = req.body;
 
+    if (title && text) {
+        //variable for the new object will save
+        const newNote = {
+            title,
+            text,
+            note_id: uniqid(),
+        };
 
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedNotes = JSON.parse(data);
 
+                //add a new note
+                parsedNotes.push(newNote);
+
+                fs.writeFileSync(
+                    './db/db.json',
+                    JSON.stringify(parsedNotes, null, 4),
+                    (writeErr) =>
+                    writeErr
+                    ? console.error(writeErr)
+                    : console.info('Successfully updated notes!')
+                );
+            }
+        });
+
+        const message = {
+            status: 'success',
+            body: newNote,
+        };
+        res.json(message);
+    } else {
+        res.json('Error in adding new note');
+    }
 });
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
