21813e4871642dd1cf0e30000a1bff57d7de0131
kstensing <stensingk@gmail.com>
2022-03-05 20:47:43 -0600

    added first two get requests

--- server.js
+++ server.js
@@ -1,27 +1,36 @@
 const express = require('express');
-const res = require('express/lib/response');
+const fs = require('fs');
+const path = require('path');
+//const res = require('express/lib/response');
+
+const PORT = 3001;
 
 const app = express();
 
-app.get('/api/notes', (req, res) => {
-    console.log("here")
-    res.json({ // saved notes? })
-        // if we want to send a file back res.render('index')
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.post('/api/notes', (req, res) => {
-    
-})
+app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../notes.html'));
+    res.sendFile(path.join(__dirname, '/develop/public/notes.html'));
 });
 
-//not sure on the path
+// not sure on the path
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'));
+    res.sendFile(path.join(__dirname, '/develop/public/index.html'));
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-});
+// app.get('/api/notes', (req, res) => {
+//     console.log("here")
+//     res.json({ // saved notes? })
+//         // if we want to send a file back res.render('index')
+// });
+
+// app.post('/api/notes', (req, res) => {
+
+// });
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
