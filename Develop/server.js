5c4c835cdd065f31718bbefaf5e764f2cacaefb5
kstensing <stensingk@gmail.com>
2022-03-05 22:14:00 -0600

    adjusted project so that notes appear on the left

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const noteData = require('./db/db.json');
+const uuid = require('uuid');
+
 //const res = require('express/lib/response');
 
 const PORT = 3001;
@@ -13,24 +16,23 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/develop/public/notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-// not sure on the path
+// one or the other:
+app.get('/api/notes', (req, res) => res.json(noteData));
+
+app.post('/api/notes', (req, res) => {
+    res.json(req.body);
+
+
+
+
+});
+
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/develop/public/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
-
-// app.get('/api/notes', (req, res) => {
-//     console.log("here")
-//     res.json({ // saved notes? })
-//         // if we want to send a file back res.render('index')
-// });
-
-// app.post('/api/notes', (req, res) => {
-
-// });
-
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
