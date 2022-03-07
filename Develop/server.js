b61ec0f757a48618086fb210a9b296f5980d19b1
kstensing <stensingk@gmail.com>
2022-03-06 18:36:52 -0600

    completed delete functionality

--- Develop/server.js
+++ Develop/server.js
@@ -11,7 +11,9 @@ const PORT = 3001;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 
 app.use(express.static('public'));
 
@@ -19,29 +21,73 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-// one or the other:
+
 app.get('/api/notes', (req, res) => {
-    
+
     fs.readFile('./db/db.json', 'utf8', (err, noteData) => {
         if (err) {
             console.error(err);
-        } 
-        
+        }
         const allNotes = JSON.parse(noteData);
         res.json(allNotes);
     });
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+
+    if (req.params.id) {
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedNotes = JSON.parse(data);
+
+                //add a new note
+                //parsedNotes.push(newNote);
+
+                const noteId = req.params.id;
+                var noteToBeDeleted = '';
+                for (let i = 0; i < parsedNotes.length; i++) {
+
+                    const currentNote = parsedNotes[i];
+                    if (currentNote.id === noteId) {
+                        noteToBeDeleted = i;
+                    }
+                }
+                if (noteToBeDeleted === '') {
+                    console.error('That ID doesn\'t exist');
+                    return;
+                }
+                res.json(noteToBeDeleted);
+                parsedNotes.splice(noteToBeDeleted, 1)
+
+                fs.writeFileSync(
+                    './db/db.json',
+                    JSON.stringify(parsedNotes, null, 4),
+                    (writeErr) =>
+                    writeErr ?
+                    console.error(writeErr) :
+                    console.info('Successfully updated notes!')
+                );
+                return;
+            }
+        })
+    }
+});
+
 app.post('/api/notes', (req, res) => {
 
-    const { title, text } = req.body;
+    const {
+        title,
+        text
+    } = req.body;
 
     if (title && text) {
         //variable for the new object will save
         const newNote = {
             title,
             text,
-            note_id: uniqid(),
+            id: uniqid(),
         };
 
         fs.readFile('./db/db.json', 'utf8', (err, data) => {
@@ -57,9 +103,9 @@ app.post('/api/notes', (req, res) => {
                     './db/db.json',
                     JSON.stringify(parsedNotes, null, 4),
                     (writeErr) =>
-                    writeErr
-                    ? console.error(writeErr)
-                    : console.info('Successfully updated notes!')
+                    writeErr ?
+                    console.error(writeErr) :
+                    console.info('Successfully updated notes!')
                 );
             }
         });
@@ -79,5 +125,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
